afb7e9105be1fd6c550b15b5db2816ee4b39698a
Ryo Murakami <ryoyuki6@yahoo.co.jp>
2024-07-23 16:19:03 +0900

    use gorilla and add timestamp

--- handler/router.go
+++ handler/router.go
@@ -1,10 +1,32 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
-func Router(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!!!")
+type responseMessage struct {
+	Status	string `json:"status"`
+	Massage	string `json:"message"`
+	Timestamp	string `json:"timestamp"`
+}
+
+func Router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", hello).Methods("GET")
+	return r
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf(">>> GET")
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	resp := responseMessage{Status: "ok", Massage: "Hello World !!!", Timestamp: currentTime}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resp)
+	log.Printf("<<<(%d)%v", http.StatusOK, resp)
+	return
 }
